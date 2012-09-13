f88d93f6e22740644581b47ec655d01b55f8f8f1
Damarus <dam4rusxp@googlemail.com>
2012-09-14 00:40:02 +0200

    Push asynchronousity to the limit! Also disable refresh button while refreshing (needs polishing) and removed single-line function

--- gadget/js/gadget.js
+++ gadget/js/gadget.js
@@ -1,32 +1,41 @@
 ﻿var fields = new Array("playerCount", "serverName", "pluginVersion", "serverVersion", "mem");
 
+var untilFinish = 0;
+
 function init() {
     System.Gadget.settingsUI = "settings.html";
     System.Gadget.onSettingsClosed = settingsClosed;
-    
+
     System.Gadget.Flyout.file = "flyout.html";
     System.Gadget.Flyout.onShow = showFlyout;
     System.Gadget.Flyout.onHide = hideFlyout;
-    
+
     settingsChanged();
 }
 
-function changeBg(newBg) {
-    System.Gadget.background = "img/" + newBg;
-}
-
 function refresh() {
+    // Disable refresh button until we finish, refresh timeout is 5s
+    System.Gadget.document.getElementById("refreshBtn").disabled = true;
+    setTimeout(function() {
+        System.Gadget.document.getElementById("refreshBtn").disabled = false;
+    }, 5 * 1000);
+
     // Load info for each existing field
+    untilFinish = fields.length;
     for(i = 0; i < fields.length; i++) {
-        sendQuery(fields[i], function(resp) {
-            System.Gadget.document.getElementById(fields[i]).innerHTML = resp;
+        if(mySettings.useCustomName && fields[i] == "serverName") continue;
+
+        sendQuery(fields[i], function(response, query) {
+            // Note: Do not access variables of the refresh function here, because it will finish before this is called
+            System.Gadget.document.getElementById(query).innerHTML = response;
+
+            untilFinish--;
+            if(untilFinish == 0) {
+                System.Gadget.document.getElementById("refreshBtn").disabled = false;
+            }
         });
     }
-    
-    if(mySettings.useCustomName) {
-        System.Gadget.document.getElementById("serverName").innerHTML = mySettings.serverName;
-    }
-    
+
     if(mySettings.useAutoRefresh) {
         setTimeout("refresh()", mySettings.refreshInterval);
     }
@@ -34,32 +43,31 @@ function refresh() {
 
 function sendQuery(query, callback) {
     // If no host is specified, do not try to update
-    if(mySettings.host != "") {
+    if(mySettings.host != undefined && mySettings.host != "") {
         var xhr = new XMLHttpRequest();
         var response;
-        
+        var field = query;
+
         // If existing, add adminPw to query
         if(mySettings.adminPw != undefined && mySettings.adminPw != "") {
-        	query += "?adminPw=" + mySettings.adminPw;
-    	}
-        xhr.open("GET", "http://" + mySettings.host + "/" + query + "?rnd=" + Math.random(), false);
-        
+            query += "?adminPw=" + mySettings.adminPw;
+        }
+        xhr.open("GET", "http://" + mySettings.host + "/" + query + "?rnd=" + Math.random(), true);
+
         // Set the function that is executes when we get an answer
         xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4) {
+            if(xhr.readyState == 4) {
                 response = xhr.responseText;
                 if(response == undefined) response = "";
 
-                typeof callback == "function" && callback(response);
+                typeof callback == "function" && callback(response, field);
             }
         }
-        
-        xhr.send(null);
-        return;
-    }
 
-    // Callback with an empty string if we get no answer
-    typeof callback == "function" && callback("");
+        xhr.send(null);
+    } else {
+        typeof callback == "function" && callback("");
+    }
 }
 
 function showFlyout() {
@@ -71,7 +79,7 @@ function hideFlyout() {
 }
 
 function settingsClosed(event) {
-    if(event.closeAction == event.Action.commit) {  
+    if(event.closeAction == event.Action.commit) {
         settingsChanged();
     }
 }
@@ -79,6 +87,10 @@ function settingsClosed(event) {
 function settingsChanged() {
     mySettings.load();
 
-    changeBg(mySettings.bg);
+    if(mySettings.useCustomName) {
+        System.Gadget.document.getElementById("serverName").innerHTML = mySettings.serverName;
+    }
+
+    System.Gadget.background = "img/" + mySettings.bg;
     refresh();
 }
